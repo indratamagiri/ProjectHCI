dc26d20bd49239555a2627e98f6cf4126a610893
indratamagiri <madegiri1212@gmail.com>
2018-12-18 00:00:11 +0700

    Slider Gambar

--- app/src/main/java/com/example/madeg/projecthci/HomeActivity.java
+++ app/src/main/java/com/example/madeg/projecthci/HomeActivity.java
@@ -9,7 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class HomeActivity extends AppCompatActivity {
     TextView textWeloome;
@@ -17,6 +19,7 @@ public class HomeActivity extends AppCompatActivity {
     Button AboutUs;
     Toolbar toolbar;
     SharedPreferences sharedPreferences;
+    ViewFlipper viewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,8 @@ public class HomeActivity extends AppCompatActivity {
         textWeloome = findViewById(R.id.welcomeHome);
         toolbar = findViewById(R.id.toolbarHome);
         AboutUs = findViewById(R.id.buttonAbout);
+        viewFlipper = findViewById(R.id.flipper);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
@@ -32,14 +37,15 @@ public class HomeActivity extends AppCompatActivity {
 
         textWeloome.setText("Welcome " +sharedPreferences.getString("username", "null"));
 
+        int images[] = {R.drawable.crazy_dough_stuffed_bread, R.drawable.french_bread, R.drawable.white_bread, R.drawable.scali_bread};
+
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, ItemActivity.class);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("username", "null");
                 editor.commit();
-                startActivity(i);
+                finish();
             }
         });
 
@@ -50,5 +56,28 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        for (int image: images){
+            flipperImage(image);
+        }
+        viewFlipper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, ItemActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void flipperImage(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
