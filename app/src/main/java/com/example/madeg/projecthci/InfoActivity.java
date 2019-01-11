2b341b15ff24634a91933177f1fc28562564df25
indratamagiri <madegiri1212@gmail.com>
2019-01-11 14:03:19 +0700

    lagi dikit

--- app/src/main/java/com/example/madeg/projecthci/InfoActivity.java
+++ app/src/main/java/com/example/madeg/projecthci/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.example.madeg.projecthci;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +16,10 @@ import android.widget.Toolbar;
 public class InfoActivity extends AppCompatActivity {
     android.support.v7.widget.Toolbar toolbar;
     ImageButton exit, back;
+    Button feedback;
+    EditText inputFeedback;
+    AlertDialog.Builder alertBuilder;
+    String mes="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +27,8 @@ public class InfoActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbarAbout);
         exit = findViewById(R.id.btnLogout);
         back = findViewById(R.id.btnBack);
-        Button feedback = findViewById(R.id.BtnFeedBack);
-        final EditText inputFeedback = findViewById(R.id.edit_text);
+        feedback = findViewById(R.id.BtnFeedBack);
+        inputFeedback = findViewById(R.id.edit_text);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
@@ -46,11 +52,20 @@ public class InfoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String input = inputFeedback.getText().toString();
                 if (input.length() < 25){
-
+                    mes = "Feedback more that 25";
                 }else{
-
+                    mes = "Thank you for your feedback";
                 }
 
+                alertBuilder = new AlertDialog.Builder(InfoActivity.this);
+                alertBuilder.setMessage(mes);
+                alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertBuilder.show();
             }
         });
     }
--- app/src/main/java/com/example/madeg/projecthci/MainActivity.java
+++ app/src/main/java/com/example/madeg/projecthci/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.madeg.projecthci;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     EditText email, password;
     Button login;
     String emailUser, passwordUser;
+    AlertDialog.Builder alertBuilder;
+    String mes ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 emailUser = email.getText().toString().trim();
                 passwordUser = password.getText().toString().trim();
-
+                mes = "";
                 if (!emailUser.isEmpty() && !passwordUser.isEmpty()) {
-                    if (isValidEmailId(emailUser)) {
+                    if (emailUser.length() > 6) {
                         SharedPreferences sharedPreferences = getSharedPreferences("dataUser", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.putString("username", emailUser);
@@ -41,23 +45,25 @@ public class MainActivity extends AppCompatActivity {
                         Intent i = new Intent(MainActivity.this, HomeActivity.class);
                         startActivity(i);
                     }else {
-                        Toast.makeText(getApplicationContext(), "you must input email", Toast.LENGTH_SHORT).show();
+                        mes = "Username must more that 6";
                     }
                 }else {
-                    Toast.makeText(getApplicationContext(), "Email and Password must be input", Toast.LENGTH_SHORT).show();
+                    mes = "Username and Password must be field";
+                }
+                if (mes.equals("")){
+                    alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertBuilder.setMessage(mes);
+                    alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    alertBuilder.show();
                 }
             }
         });
 
 
-    }
-    private boolean isValidEmailId(String email){
-
-        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
     }
 }
